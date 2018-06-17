e10e087b1dc8f05e2d03d6b413364403d7e40eb8
tth <tangpatricktw2@gmail.com>
2018-06-17 22:33:13 +0800

    add config.ini

--- bot_main.py
+++ bot_main.py
@@ -14,8 +14,8 @@ import ptt, configparser
 
 app = Flask(__name__)
 
-config = configparser.ConfigParser()
-config.read('config.ini')
+#config = configparser.ConfigParser()
+#config.read('config.ini')
 
 line_bot_api = LineBotApi('21966c724075b8636306fb9d7f47201a')
 handler = WebhookHandler('BuhgBcv9EZuqq4enk25URlqliZiOVvU8DSddmNzEz7UGJWLITA9RHMZAaX/B/2vNoQ1JK+C4ABG0JvZ76mBZYw2wVjRzzhdBytaD6lqGd3JcaY2eO19M8ti6BacaitPfX54231wsRAjKHYoR27934QdB04t89/1O/w1cDnyilFU=')
