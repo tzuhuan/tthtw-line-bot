3b471c3b4c9436acf57583a7ad7fbd3c9e37d28e
tth <tangpatricktw2@gmail.com>
2018-06-17 22:24:12 +0800

    add config.ini

--- bot_main.py
+++ bot_main.py
@@ -10,12 +10,15 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage, StickerMessage, StickerSendMessage,
 )
 
-import ptt
+import ptt, configparser
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('BuhgBcv9EZuqq4enk25URlqliZiOVvU8DSddmNzEz7UGJWLITA9RHMZAaX/B/2vNoQ1JK+C4ABG0JvZ76mBZYw2wVjRzzhdBytaD6lqGd3JcaY2eO19M8ti6BacaitPfX54231wsRAjKHYoR27934QdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('21966c724075b8636306fb9d7f47201a')
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+line_bot_api = LineBotApi(config['tthtw-line-bot']['Channel_Secret'])
+handler = WebhookHandler(config['tthtw-line-bot']['Channel_Access_Token'])
 
 
 @app.route("/callback", methods=['POST'])
