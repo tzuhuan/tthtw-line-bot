081f2f486ee30d30c033653dfa02cf1c0bad9998
tzuhuan <tangpatricktw2@gmail.com>
2018-06-20 22:44:17 -0700

    filter out gif and png

--- randomcat.py
+++ randomcat.py
@@ -3,22 +3,33 @@ import requests, bs4
 RANDOM_CAT = 'http://random.cat/'
 
 class RandomCat():
-    def query(self, url = RANDOM_CAT):   
-        res = requests.get(url)
-        try:
-            res.raise_for_status()
-        except:
-            return RANDOM_CAT
+    def query(self, url = RANDOM_CAT):
+        image_url = RANDOM_CAT
+        stop = False
+        
+        while not stop:
+            res = requests.get(url)
+            try:
+                res.raise_for_status()
+            except:
+                break;
             
-        bs = bs4.BeautifulSoup(res.text, 'html.parser')
-        links = bs.select('#cat')
+            bs = bs4.BeautifulSoup(res.text, 'html.parser')
+            tags = bs.select('#cat')
+            link = tags[0].get('src')
+            
+            if link.find('.gif') != -1 or link.find('.png') != -1:
+                continue
+            
+            image_url = link
+            stop = True
         
-        return links[0].get('src')
-        
+        return image_url     
 
 def test():
     cat = RandomCat()
     print(cat.query())
     
 if __name__ == '__main__':
-    test()
+    for i in range(20):
+        test()
